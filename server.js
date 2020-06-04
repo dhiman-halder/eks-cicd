ea74821bd86a5bba96d02b591b76eb0210520f40
dhiman-halder <dhiman.halder@gmail.com>
2020-06-03 22:57:29 -0700

    updated code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req,res) {
-    res.send('Hello from the Web app!!!!!');
+    res.send('Hello from the Web app!');
 });
 
 app.listen(8080, ()=> console.log('listening on 8080'))
